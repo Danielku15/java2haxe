747a9f99ceb54c99af316c0e200af94a4ccc9984
Danielku15 <danielku15@coderline.net>
2012-09-03 08:08:56 +0200

    Some copyright/license stuff, some documentation

--- src/java2haxe/Converter.java
+++ src/java2haxe/Converter.java
@@ -1,9 +1,25 @@
+/*
+ * This file is part of java2haxe.
+ *
+ *  java2haxe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  alphaTab is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with java2haxe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package java2haxe;
 
 /**
  * This small command-line utility allows the conversion of a XMI file created
- * by Modisc to Haxe source. Simply convert any Java project to a XMI file using
- * the Modisc plugin and run this converter to get Haxe source.
+ * by MoDisco to Haxe source. Simply convert any Java project to a XMI file
+ * using the MoDisco plugin and run this converter to get Haxe source.
  */
 public class Converter
 {
@@ -18,7 +34,7 @@ public class Converter
         try
         {
             // initialize the processor
-            ModiscXmiToHaxeProcessor processor = new ModiscXmiToHaxeProcessor();
+            MoDiscoXmiToHaxeProcessor processor = new MoDiscoXmiToHaxeProcessor();
             processor.setInputXmi(args[0]);
             processor.setTargetDir(args[1]);
             processor.run();
@@ -36,9 +52,9 @@ public class Converter
         System.out
                 .println("  This small command-line utility allows the conversion of a XMI file created");
         System.out
-                .println("  by Modisc to Haxe source. Simply convert any Java project to a XMI file using");
+                .println("  by MoDisco to Haxe source. Simply convert any Java project to a XMI file using");
         System.out
-                .println("  the Modisc plugin and run this converter to get Haxe source.");
+                .println("  the MoDisco plugin and run this converter to get Haxe source.");
         System.out.println("Usage:");
         System.out.println("  java2haxe.Converter [xmi-file] [output-dir]");
     }
--- src/java2haxe/MoDiscoXmiToHaxeProcessor.java
+++ src/java2haxe/MoDiscoXmiToHaxeProcessor.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of java2haxe.
+ *
+ *  java2haxe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  alphaTab is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with java2haxe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package java2haxe;
 
 import java.io.File;
@@ -100,13 +116,13 @@ import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.eclipse.gmt.modisco.java.emf.JavaPackage;
 
 /**
- * This processor takes the path to an XMI file created by Modisc and an output
+ * This processor takes the path to an XMI file created by MoDisco and an output
  * directory. Using the {@link Model} serialized in the XMI plain Haxe source is
  * generated.
  * 
  * TODO: Maybe we should replace this processor with an Xtend class.
  */
-public class ModiscXmiToHaxeProcessor
+public class MoDiscoXmiToHaxeProcessor
 {
     private String            _inputXmi;
     private String            _targetDir;
